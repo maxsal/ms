ee201c43d1caa2ca642697e124586e12de60b548
Max Salvatore <maxsalvatore@Maxs-MacBook-Air.local>
2023-08-04 14:49:02 -0400

    fix merge_list

--- R/merge_list.R
+++ R/merge_list.R
@@ -1,11 +1,13 @@
 #' Quickly merge across a list using dplyr-based merge functions
-#' @param ... a list object 
+#' @param data_list a list object of data frames to merge
 #' @param join_fn the function on which to join (default = dplyr::left_join)
 #' @param by_var the name of the variable(s) on which to join
 #' @param verbose logical; report merging variable and merge function
 #' @return a merged table across objects in list ...
 #' @importFrom cli cli_alert_warning
 #' @importFrom cli cli_text
+#' @importFrom data.table as.data.table
+#' @importFrom data.table is.data.table
 #' @importFrom dplyr full_join
 #' @export
 #' @examples
@@ -24,8 +26,9 @@
 #' merged
 #' }
 
-merge_list <- function(..., join_fn = NULL, by_var = "id", verbose = FALSE) {
-    if (!by_var %in% names(...[[1]])) {
+merge_list <- function(data_list, join_fn = NULL, by_var = "id", verbose = FALSE) {
+    if (!is.data.table(data_list[[1]])) data_list <- lapply(data_list, data.table::as.data.table)
+    if (!by_var %in% names(data_list[[1]])) {
         cli::cli_alert_warning("{.field {by_var}} not in names of dataset")
         stop("change 'by_var' argument")
     }
@@ -35,5 +38,5 @@ merge_list <- function(..., join_fn = NULL, by_var = "id", verbose = FALSE) {
     } else if (verbose) {
         cli::cli_text("merging on {.field {by_var}} using {.fun {deparse(substitute(join_fn))}}")
     }
-    Reduce(\(x, y) join_fn(x, y, by = by_var), ...)
+    Reduce(\(x, y) join_fn(x, y, by = by_var), data_list)
 }
